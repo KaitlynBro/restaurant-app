49c0e9d9be0c8162a15031caf7cce1f365711c05
Kaitlyn Brouwers <kaitlynnicoleb@hotmail.com>
2017-08-31 18:28:15 -0400

    adding js

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,6 +1,12 @@
-
 //smooth scroll for menu section
 $('#menuButton').on('click', function() {
+    $('html,body').animate({
+        scrollTop: $('.menuContainer').offset().top},
+        'slow');
+});
+
+//smooth scroll for menu section
+$('#seeMoreButton').on('click', function() {
     $('html,body').animate({
         scrollTop: $('.menuContainer').offset().top},
         'slow');
@@ -27,10 +33,10 @@ $('#contactButton').on('click', function() {
         'slow');
 });
 
-//preventing page refresh on submit
-$('#submitButton').click(function(e) {
+// //preventing page refresh on submit
+$('#submitButton').on('submit', function(e) {
     e.preventDefault();
-});
+})
 
 const monday = document.getElementById('monday');
 const tuesday = document.getElementById('tuesday');
@@ -38,34 +44,72 @@ const wednesday = document.getElementById('wednesday');
 const thursday = document.getElementById('thursday');
 const saturday = document.getElementById('saturday');
 const sunday = document.getElementById('sunday');
-
+const mondayImage = document.getElementById('pic1');
 
 $(monday).on('click', function() {
-    $('specialsGallery').empty();
+    document.getElementById('pic6').style.display = 'none';
+    document.getElementById('pic5').style.display = 'none';
+    document.getElementById('pic4').style.display = 'none';
+    document.getElementById('pic3').style.display = 'none';
     document.getElementById('pic2').style.display = 'none';
     document.getElementById('pic1').style.display = 'block';
+    $('#specialsText').empty();
+    document.getElementById('specialsText').append('$10 for Lightly Fried White Fish with Avocado Mash and Potato Wedges with Creamy Ranch Dip');
 })
 $(tuesday).on('click', function() {
     document.getElementById('pic1').style.display = 'none';
     document.getElementById('pic3').style.display = 'none';
+    document.getElementById('pic4').style.display = 'none';
+    document.getElementById('pic5').style.display = 'none';
+    document.getElementById('pic6').style.display = 'none';
     document.getElementById('pic2').style.display = 'block';
+    $('#specialsText').empty();
+    document.getElementById('specialsText').append('$15 for Peppered Salmon and Corn');
 })
 $(wednesday).on('click', function() {
     document.getElementById('pic1').style.display = 'none';
     document.getElementById('pic2').style.display = 'none';
+    document.getElementById('pic4').style.display = 'none';
+    document.getElementById('pic5').style.display = 'none';
+    document.getElementById('pic6').style.display = 'none';
     document.getElementById('pic3').style.display = 'block';
+    $('#specialsText').empty();
+    document.getElementById('specialsText').append('$4 for 1 Pint of Hollano Beer');
 })
-$
-
-
+$(thursday).on('click', function() {
+    document.getElementById('pic1').style.display = 'none';
+    document.getElementById('pic2').style.display = 'none';
+    document.getElementById('pic3').style.display = 'none';
+    document.getElementById('pic5').style.display = 'none';
+    document.getElementById('pic6').style.display = 'none';
+    document.getElementById('pic4').style.display = 'block';
+    $('#specialsText').empty();
+    document.getElementById('specialsText').append('$10 for Salmon Sushi, Soy Sauce Drizzle, and Cavier ');
+})
+$(saturday).on('click', function() {
+    document.getElementById('pic1').style.display = 'none';
+    document.getElementById('pic2').style.display = 'none';
+    document.getElementById('pic3').style.display = 'none';
+    document.getElementById('pic4').style.display = 'none';
+    document.getElementById('pic6').style.display = 'none';
+    document.getElementById('pic5').style.display = 'block';
+    $('#specialsText').empty();
+    document.getElementById('specialsText').append('$8 for any Appetizer (pictured above: Deep Fried Plaintain Bananas wrapped in Zuchini');
+})
+$(sunday).on('click', function() {
+    document.getElementById('pic1').style.display = 'none';
+    document.getElementById('pic2').style.display = 'none';
+    document.getElementById('pic3').style.display = 'none';
+    document.getElementById('pic4').style.display = 'none';
+    document.getElementById('pic5').style.display = 'none';
+    document.getElementById('pic6').style.display = 'block';
+    $('#specialsText').empty();
+    document.getElementById('specialsText').append('$5 for Any Dessert (pictured above: Frozen Fruit Cheesecake)');
+});
 
 /*need to do
 1. find DRY way to apply smooth scroll on multiple buttons
 2. find DRY way to display / hide elements in gallery images section
+4. i want to remove all jquery and use only vanilla js
 */
-
-
-
-
-
 
--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,6 +6,12 @@ $('#menuButton').on('click', function () {
         scrollTop: $('.menuContainer').offset().top }, 'slow');
 });
 
+//smooth scroll for menu section
+$('#seeMoreButton').on('click', function () {
+    $('html,body').animate({
+        scrollTop: $('.menuContainer').offset().top }, 'slow');
+});
+
 //smooth scroll for gallery section
 $('#galleryButton').on('click', function () {
     $('html,body').animate({
@@ -24,8 +30,8 @@ $('#contactButton').on('click', function () {
         scrollTop: $('#contactContainer').offset().top }, 'slow');
 });
 
-//preventing page refresh on submit
-$('#submitButton').click(function (e) {
+// //preventing page refresh on submit
+$('#submitButton').on('submit', function (e) {
     e.preventDefault();
 });
 
@@ -35,24 +41,71 @@ var wednesday = document.getElementById('wednesday');
 var thursday = document.getElementById('thursday');
 var saturday = document.getElementById('saturday');
 var sunday = document.getElementById('sunday');
+var mondayImage = document.getElementById('pic1');
 
 $(monday).on('click', function () {
-    $('specialsGallery').empty();
+    document.getElementById('pic6').style.display = 'none';
+    document.getElementById('pic5').style.display = 'none';
+    document.getElementById('pic4').style.display = 'none';
+    document.getElementById('pic3').style.display = 'none';
     document.getElementById('pic2').style.display = 'none';
     document.getElementById('pic1').style.display = 'block';
+    $('#specialsText').empty();
+    document.getElementById('specialsText').append('$10 for Lightly Fried White Fish with Avocado Mash and Potato Wedges with Creamy Ranch Dip');
 });
 $(tuesday).on('click', function () {
     document.getElementById('pic1').style.display = 'none';
     document.getElementById('pic3').style.display = 'none';
+    document.getElementById('pic4').style.display = 'none';
+    document.getElementById('pic5').style.display = 'none';
+    document.getElementById('pic6').style.display = 'none';
     document.getElementById('pic2').style.display = 'block';
+    $('#specialsText').empty();
+    document.getElementById('specialsText').append('$15 for Peppered Salmon and Corn');
 });
 $(wednesday).on('click', function () {
     document.getElementById('pic1').style.display = 'none';
     document.getElementById('pic2').style.display = 'none';
+    document.getElementById('pic4').style.display = 'none';
+    document.getElementById('pic5').style.display = 'none';
+    document.getElementById('pic6').style.display = 'none';
     document.getElementById('pic3').style.display = 'block';
+    $('#specialsText').empty();
+    document.getElementById('specialsText').append('$4 for 1 Pint of Hollano Beer');
+});
+$(thursday).on('click', function () {
+    document.getElementById('pic1').style.display = 'none';
+    document.getElementById('pic2').style.display = 'none';
+    document.getElementById('pic3').style.display = 'none';
+    document.getElementById('pic5').style.display = 'none';
+    document.getElementById('pic6').style.display = 'none';
+    document.getElementById('pic4').style.display = 'block';
+    $('#specialsText').empty();
+    document.getElementById('specialsText').append('$10 for Salmon Sushi, Soy Sauce Drizzle, and Cavier ');
+});
+$(saturday).on('click', function () {
+    document.getElementById('pic1').style.display = 'none';
+    document.getElementById('pic2').style.display = 'none';
+    document.getElementById('pic3').style.display = 'none';
+    document.getElementById('pic4').style.display = 'none';
+    document.getElementById('pic6').style.display = 'none';
+    document.getElementById('pic5').style.display = 'block';
+    $('#specialsText').empty();
+    document.getElementById('specialsText').append('$8 for any Appetizer (pictured above: Deep Fried Plaintain Bananas wrapped in Zuchini');
+});
+$(sunday).on('click', function () {
+    document.getElementById('pic1').style.display = 'none';
+    document.getElementById('pic2').style.display = 'none';
+    document.getElementById('pic3').style.display = 'none';
+    document.getElementById('pic4').style.display = 'none';
+    document.getElementById('pic5').style.display = 'none';
+    document.getElementById('pic6').style.display = 'block';
+    $('#specialsText').empty();
+    document.getElementById('specialsText').append('$5 for Any Dessert (pictured above: Frozen Fruit Cheesecake)');
 });
 
-// monday.addEventListener('click', function() {
-//     document.getElementById('pic1').style.display ='block';
-
-// })
+/*need to do
+1. find DRY way to apply smooth scroll on multiple buttons
+2. find DRY way to display / hide elements in gallery images section
+4. i want to remove all jquery and use only vanilla js
+*/
