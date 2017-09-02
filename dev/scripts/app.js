34db1dbef4b490f21ab5f4249192f14520e90c58
Kaitlyn Brouwers <kaitlynnicoleb@hotmail.com>
2017-09-02 17:47:21 -0400

    making code more dry

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,86 +1,77 @@
-const monday = document.getElementById('monday');
-const tuesday = document.getElementById('tuesday');
-const wednesday = document.getElementById('wednesday');
-const thursday = document.getElementById('thursday');
-const saturday = document.getElementById('saturday');
-const sunday = document.getElementById('sunday');
-const specialsSection = document.getElementById('specialsPictures');
-const galleryButtons = document.getElementsByClassName('galleryButton');
-const galleryImages = document.getElementsByClassName('specialsImage');
+const tastyImages = {
+    Monday: "../../assets/monday.png",
+    Tuesday: "assets/salmon.png",
+    Wednesday: "assets/beer.png",
+    Thursday: "assets/salmon.png",
+    Saturday: "assets/zuchini.png",
+    Sunday: "assets/cheesecake.png"
+};
 
+let mondayImage = tastyImages.Monday;
+let tuesdayImage = tastyImages.Tuesday;
+let wednesdayImage = tastyImages.Wednesday;
+let thursdayImage = tastyImages.Thursday;
+let saturdayImage = tastyImages.Saturday;
+let sundayImage = tastyImages.Sunday;
 
-//first method tried - cannot read property style of null
-monday.addEventListener('click', function() {
-    function OnClick() {
-        specialsSection.innerHTML = '';
-    }
-    function showContent() {
-        document.getElementById('pic1').style.display = 'block';
-    }
-})
-
-//second method tried
-//loop over buttons on click, check what button was pressed, find the matching information (ie. if monday was pressed, find monday image), empty container if any current info is there, append new info
-for (var i = 0 ; i < galleryButtons.length; i++) {
-   galleryButtons[i].addEventListener('click', function() {
-        console.log(galleryImages)
-   }); 
+function getSelectedDay() {
+  $('.galleryButton').on('click', function(e){
+      e.preventDefault();
+      document.getElementById('specialsPictures').innerHTML = $(this).val();
+    });
 }
- 
 
- 
-
-//third method, switched the event listener and the for loop around - said add event listener not a function
-galleryButtons.addEventListener('click', function() {
-    for (var i = 0; i < galleryButtons.length; i++) {
-        console.log(this.value)
-    }
-})
-
-//smooth scroll for menu section
-$('#menuButton').on('click', function() {
-    $('html,body').animate({
-        scrollTop: $('.menuContainer').offset().top},
-        'slow');
-});
-
-//smooth scroll for menu section
-$('#seeMoreButton').on('click', function() {
-    $('html,body').animate({
-        scrollTop: $('.menuContainer').offset().top},
-        'slow');
-});
-
-//smooth scroll for gallery section
-$('#galleryButton').on('click', function() {
-    $('html,body').animate({
-        scrollTop: $('#galleryContainer').offset().top},
-        'slow');
-});
-
-//smooth scroll for specials section
-$('#specialsButton').on('click', function() {
-    $('html,body').animate({
-        scrollTop: $('#specialsContainer').offset().top},
-        'slow');
-});
-
-//smooth scroll for contact section
-$('#contactButton').on('click', function() {
-    $('html,body').animate({
-        scrollTop: $('#contactContainer').offset().top},
-        'slow');
-});
-
-// //preventing page refresh on submit
-$('#submitButton').on('submit', function(e) {
+$('.galleryButton').on('click', function(e){
     e.preventDefault();
-})
+    let chosenDay = $(this).val();
+    $('.todaysSpecial').attr('src', tastyImages[chosenDay]);
+    $('#specialsPictures').empty().append(`<img src='${tastyImages[chosenDay]}'/>`)
+});
+
+// //smooth scroll for menu section
+// $('#menuButton').on('click', function() {
+//     $('html,body').animate({
+//         scrollTop: $('.menuContainer').offset().top},
+//         'slow');
+// });
+
+// //smooth scroll for menu section
+// $('#seeMoreButton').on('click', function() {
+//     $('html,body').animate({
+//         scrollTop: $('.menuContainer').offset().top},
+//         'slow');
+// });
+
+// //smooth scroll for gallery section
+// $('#galleryButton').on('click', function() {
+//     $('html,body').animate({
+//         scrollTop: $('#galleryContainer').offset().top},
+//         'slow');
+// });
+
+// //smooth scroll for specials section
+// $('#specialsButton').on('click', function() {
+//     $('html,body').animate({
+//         scrollTop: $('#specialsContainer').offset().top},
+//         'slow');
+// });
+
+// //smooth scroll for contact section
+// $('#contactButton').on('click', function() {
+//     $('html,body').animate({
+//         scrollTop: $('#contactContainer').offset().top},
+//         'slow');
+// });
+
+// // //preventing page refresh on submit
+// $('#submitButton').on('submit', function(e) {
+//     e.preventDefault();
+// })
 
 
-/*need to do
-1. find DRY way to apply smooth scroll on multiple buttons
-2. find DRY way to display / hide elements in gallery images section
-4. i want to remove all jquery and use only vanilla js
-*/
+// /*need to do
+// 1. find DRY way to apply smooth scroll on multiple buttons
+// 2. find DRY way to display / hide elements in gallery images section
+// 4. i want to remove all jquery and use only vanilla js
+// */
 
--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,77 +1,78 @@
-'use strict';
+"use strict";
 
-var monday = document.getElementById('monday');
-var tuesday = document.getElementById('tuesday');
-var wednesday = document.getElementById('wednesday');
-var thursday = document.getElementById('thursday');
-var saturday = document.getElementById('saturday');
-var sunday = document.getElementById('sunday');
-var specialsSection = document.getElementById('specialsPictures');
-var galleryButtons = document.getElementsByClassName('galleryButton');
-var galleryImages = document.getElementsByClassName('specialsImage');
+var tastyImages = {
+    Monday: "../../assets/monday.png",
+    Tuesday: "assets/salmon.png",
+    Wednesday: "assets/beer.png",
+    Thursday: "assets/salmon.png",
+    Saturday: "assets/zuchini.png",
+    Sunday: "assets/cheesecake.png"
+};
 
-//first method tried - cannot read property style of null
-monday.addEventListener('click', function () {
-    function OnClick() {
-        specialsSection.innerHTML = '';
-    }
-    function showContent() {
-        document.getElementById('pic1').style.display = 'block';
-    }
-});
+var mondayImage = tastyImages.Monday;
+var tuesdayImage = tastyImages.Tuesday;
+var wednesdayImage = tastyImages.Wednesday;
+var thursdayImage = tastyImages.Thursday;
+var saturdayImage = tastyImages.Saturday;
+var sundayImage = tastyImages.Sunday;
 
-//second method tried
-//loop over buttons on click, check what button was pressed, find the matching information (ie. if monday was pressed, find monday image), empty container if any current info is there, append new info
-for (var i = 0; i < galleryButtons.length; i++) {
-    galleryButtons[i].addEventListener('click', function () {
-        console.log(galleryImages);
+function getSelectedDay() {
+    $('.galleryButton').on('click', function (e) {
+        e.preventDefault();
+        document.getElementById('specialsPictures').innerHTML = $(this).val();
     });
 }
 
-//third method, switched the event listener and the for loop around - said add event listener not a function
-galleryButtons.addEventListener('click', function () {
-    for (var i = 0; i < galleryButtons.length; i++) {
-        console.log(this.value);
-    }
-});
-
-//smooth scroll for menu section
-$('#menuButton').on('click', function () {
-    $('html,body').animate({
-        scrollTop: $('.menuContainer').offset().top }, 'slow');
-});
-
-//smooth scroll for menu section
-$('#seeMoreButton').on('click', function () {
-    $('html,body').animate({
-        scrollTop: $('.menuContainer').offset().top }, 'slow');
-});
-
-//smooth scroll for gallery section
-$('#galleryButton').on('click', function () {
-    $('html,body').animate({
-        scrollTop: $('#galleryContainer').offset().top }, 'slow');
-});
-
-//smooth scroll for specials section
-$('#specialsButton').on('click', function () {
-    $('html,body').animate({
-        scrollTop: $('#specialsContainer').offset().top }, 'slow');
-});
-
-//smooth scroll for contact section
-$('#contactButton').on('click', function () {
-    $('html,body').animate({
-        scrollTop: $('#contactContainer').offset().top }, 'slow');
-});
-
-// //preventing page refresh on submit
-$('#submitButton').on('submit', function (e) {
+$('.galleryButton').on('click', function (e) {
     e.preventDefault();
+    var chosenDay = $(this).val();
+    $('.todaysSpecial').attr('src', tastyImages[chosenDay]);
+    $('#specialsPictures').empty().append("<img src='" + tastyImages[chosenDay] + "'/>");
 });
 
-/*need to do
-1. find DRY way to apply smooth scroll on multiple buttons
-2. find DRY way to display / hide elements in gallery images section
-4. i want to remove all jquery and use only vanilla js
-*/
+// //smooth scroll for menu section
+// $('#menuButton').on('click', function() {
+//     $('html,body').animate({
+//         scrollTop: $('.menuContainer').offset().top},
+//         'slow');
+// });
+
+// //smooth scroll for menu section
+// $('#seeMoreButton').on('click', function() {
+//     $('html,body').animate({
+//         scrollTop: $('.menuContainer').offset().top},
+//         'slow');
+// });
+
+// //smooth scroll for gallery section
+// $('#galleryButton').on('click', function() {
+//     $('html,body').animate({
+//         scrollTop: $('#galleryContainer').offset().top},
+//         'slow');
+// });
+
+// //smooth scroll for specials section
+// $('#specialsButton').on('click', function() {
+//     $('html,body').animate({
+//         scrollTop: $('#specialsContainer').offset().top},
+//         'slow');
+// });
+
+// //smooth scroll for contact section
+// $('#contactButton').on('click', function() {
+//     $('html,body').animate({
+//         scrollTop: $('#contactContainer').offset().top},
+//         'slow');
+// });
+
+// // //preventing page refresh on submit
+// $('#submitButton').on('submit', function(e) {
+//     e.preventDefault();
+// })
+
+
+// /*need to do
+// 1. find DRY way to apply smooth scroll on multiple buttons
+// 2. find DRY way to display / hide elements in gallery images section
+// 4. i want to remove all jquery and use only vanilla js
+// */
