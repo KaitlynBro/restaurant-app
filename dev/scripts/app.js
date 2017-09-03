5a05975ac4d23aa585fe71815644ad0003652964
Kaitlyn Brouwers <kaitlynnicoleb@hotmail.com>
2017-09-03 12:16:17 -0400

    more js updates

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -2,7 +2,7 @@ const tastyImages = {
     Monday: "../../assets/monday.png",
     Tuesday: "assets/salmon.png",
     Wednesday: "assets/beer.png",
-    Thursday: "assets/salmon.png",
+    Thursday: "assets/sushi.png",
     Saturday: "assets/zuchini.png",
     Sunday: "assets/cheesecake.png"
 };
@@ -14,64 +14,56 @@ let thursdayImage = tastyImages.Thursday;
 let saturdayImage = tastyImages.Saturday;
 let sundayImage = tastyImages.Sunday;
 
-function getSelectedDay() {
-  $('.galleryButton').on('click', function(e){
-      e.preventDefault();
-      document.getElementById('specialsPictures').innerHTML = $(this).val();
-    });
-}
-
 $('.galleryButton').on('click', function(e){
     e.preventDefault();
     let chosenDay = $(this).val();
     $('.todaysSpecial').attr('src', tastyImages[chosenDay]);
-    $('#specialsPictures').empty().append(`<img src='${tastyImages[chosenDay]}'/>`)
+    $('#specialsPictures').empty().append(`<img src='${tastyImages[chosenDay]}'/>`);
 });
 
-// //smooth scroll for menu section
-// $('#menuButton').on('click', function() {
-//     $('html,body').animate({
-//         scrollTop: $('.menuContainer').offset().top},
-//         'slow');
-// });
+//smooth scroll for menu section
+$('#menuButton').on('click', function() {
+    $('html,body').animate({
+        scrollTop: $('.menuContainer').offset().top},
+        'slow');
+});
 
-// //smooth scroll for menu section
-// $('#seeMoreButton').on('click', function() {
-//     $('html,body').animate({
-//         scrollTop: $('.menuContainer').offset().top},
-//         'slow');
-// });
+//smooth scroll for menu section
+$('#seeMoreButton').on('click', function() {
+    $('html,body').animate({
+        scrollTop: $('.menuContainer').offset().top},
+        'slow');
+});
 
-// //smooth scroll for gallery section
-// $('#galleryButton').on('click', function() {
-//     $('html,body').animate({
-//         scrollTop: $('#galleryContainer').offset().top},
-//         'slow');
-// });
+//smooth scroll for gallery section
+$('#galleryButton').on('click', function() {
+    $('html,body').animate({
+        scrollTop: $('#galleryContainer').offset().top},
+        'slow');
+});
 
-// //smooth scroll for specials section
-// $('#specialsButton').on('click', function() {
-//     $('html,body').animate({
-//         scrollTop: $('#specialsContainer').offset().top},
-//         'slow');
-// });
+//smooth scroll for specials section
+$('#specialsButton').on('click', function() {
+    $('html,body').animate({
+        scrollTop: $('#specialsContainer').offset().top},
+        'slow');
+});
 
-// //smooth scroll for contact section
-// $('#contactButton').on('click', function() {
-//     $('html,body').animate({
-//         scrollTop: $('#contactContainer').offset().top},
-//         'slow');
-// });
+//smooth scroll for contact section
+$('#contactButton').on('click', function() {
+    $('html,body').animate({
+        scrollTop: $('#contactContainer').offset().top},
+        'slow');
+});
 
-// // //preventing page refresh on submit
-// $('#submitButton').on('submit', function(e) {
-//     e.preventDefault();
-// })
+// //preventing page refresh on submit
+$('#submitButton').on('submit', function(e) {
+    e.preventDefault();
+})
 
 
 // /*need to do
 // 1. find DRY way to apply smooth scroll on multiple buttons
-// 2. find DRY way to display / hide elements in gallery images section
 // 4. i want to remove all jquery and use only vanilla js
 // */
 
--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,7 +4,7 @@ var tastyImages = {
     Monday: "../../assets/monday.png",
     Tuesday: "assets/salmon.png",
     Wednesday: "assets/beer.png",
-    Thursday: "assets/salmon.png",
+    Thursday: "assets/sushi.png",
     Saturday: "assets/zuchini.png",
     Sunday: "assets/cheesecake.png"
 };
@@ -16,13 +16,6 @@ var thursdayImage = tastyImages.Thursday;
 var saturdayImage = tastyImages.Saturday;
 var sundayImage = tastyImages.Sunday;
 
-function getSelectedDay() {
-    $('.galleryButton').on('click', function (e) {
-        e.preventDefault();
-        document.getElementById('specialsPictures').innerHTML = $(this).val();
-    });
-}
-
 $('.galleryButton').on('click', function (e) {
     e.preventDefault();
     var chosenDay = $(this).val();
@@ -30,49 +23,42 @@ $('.galleryButton').on('click', function (e) {
     $('#specialsPictures').empty().append("<img src='" + tastyImages[chosenDay] + "'/>");
 });
 
-// //smooth scroll for menu section
-// $('#menuButton').on('click', function() {
-//     $('html,body').animate({
-//         scrollTop: $('.menuContainer').offset().top},
-//         'slow');
-// });
+//smooth scroll for menu section
+$('#menuButton').on('click', function () {
+    $('html,body').animate({
+        scrollTop: $('.menuContainer').offset().top }, 'slow');
+});
 
-// //smooth scroll for menu section
-// $('#seeMoreButton').on('click', function() {
-//     $('html,body').animate({
-//         scrollTop: $('.menuContainer').offset().top},
-//         'slow');
-// });
+//smooth scroll for menu section
+$('#seeMoreButton').on('click', function () {
+    $('html,body').animate({
+        scrollTop: $('.menuContainer').offset().top }, 'slow');
+});
 
-// //smooth scroll for gallery section
-// $('#galleryButton').on('click', function() {
-//     $('html,body').animate({
-//         scrollTop: $('#galleryContainer').offset().top},
-//         'slow');
-// });
+//smooth scroll for gallery section
+$('#galleryButton').on('click', function () {
+    $('html,body').animate({
+        scrollTop: $('#galleryContainer').offset().top }, 'slow');
+});
 
-// //smooth scroll for specials section
-// $('#specialsButton').on('click', function() {
-//     $('html,body').animate({
-//         scrollTop: $('#specialsContainer').offset().top},
-//         'slow');
-// });
+//smooth scroll for specials section
+$('#specialsButton').on('click', function () {
+    $('html,body').animate({
+        scrollTop: $('#specialsContainer').offset().top }, 'slow');
+});
 
-// //smooth scroll for contact section
-// $('#contactButton').on('click', function() {
-//     $('html,body').animate({
-//         scrollTop: $('#contactContainer').offset().top},
-//         'slow');
-// });
+//smooth scroll for contact section
+$('#contactButton').on('click', function () {
+    $('html,body').animate({
+        scrollTop: $('#contactContainer').offset().top }, 'slow');
+});
 
-// // //preventing page refresh on submit
-// $('#submitButton').on('submit', function(e) {
-//     e.preventDefault();
-// })
-
+// //preventing page refresh on submit
+$('#submitButton').on('submit', function (e) {
+    e.preventDefault();
+});
 
 // /*need to do
 // 1. find DRY way to apply smooth scroll on multiple buttons
-// 2. find DRY way to display / hide elements in gallery images section
 // 4. i want to remove all jquery and use only vanilla js
 // */
